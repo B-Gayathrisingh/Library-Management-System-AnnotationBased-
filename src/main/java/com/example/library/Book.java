6f355ae5be625147114e00b02ecf7d25de744e86
B-Gayathrisingh <gayathrisingh810@gmail.com>
2025-07-30 20:02:43 +0530

    programms

---
+++ src/main/java/com/example/library/Book.java
@@ -0,0 +1,19 @@
+package com.example.library;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+   
+	public void searchBook() {
+		 System.out.println("Searching book in catalog.");
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addBook() {
+		  System.out.println("Book added to catalog.");
+    }
+		// TODO Auto-generated method stub
+
+}
---
+++ src/main/java/com/example/library/LibraryService.java
@@ -0,0 +1,26 @@
+package com.example.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryService {
+
+    @Autowired
+    private Book book;
+
+    @Autowired
+    private Member member;
+
+    @Autowired
+    private Loan loan;
+
+    public void manageLibrary() {
+        book.addBook();
+        book.searchBook();
+        member.registerMember();
+        member.viewMembers();
+        loan.issueBook();
+        loan.returnBook();
+    }
+}
